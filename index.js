ffb61dec6dc1b82f66b8f527e9492c96706a1a3b
Diogo Moreira <diogo.dmoreira@gmail.com>
2021-11-18 15:01:58 -0300

    #1 Resolvendo problema de comparação em caso de números em ordem decrescente

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function find(...nums) {
   let largest = Number.MIN_VALUE;
   for (const n of nums) {
     if (n < smallest) smallest = n;
-    else if (n > largest) largest = n;
+    if (n > largest) largest = n;
   }
   return [smallest, largest];
 }
 
-console.log(find(4, 25, 7, 9));
-console.log(find(4, 3, 2, 1));
+console.log(find(4, 25, 7, 9)); // Dá certo
+console.log(find(4, 3, 2, 1)); // Não dá certo
